04fd22abb763a410434211eeb058fe2e7248a8b2
Neha.Nain <neha.nain@stackroute.in>
2022-09-14 18:17:41 +0530

    init

---
+++ slot-creation-server/src/main/java/com/stackroute/SlotCreationServerApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlotCreationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlotCreationServerApplication.class, args);
+	}
+
+}
